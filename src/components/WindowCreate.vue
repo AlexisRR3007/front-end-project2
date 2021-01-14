ceebab2a408a289435c7142738c37ddd9a88c86e
Fanny BESSON <fanny.besson@etu.emse.fr>
2021-01-14 18:07:05 +0100

    Resolution issue #1

--- src/components/WindowCreate.vue
+++ src/components/WindowCreate.vue
@@ -1,16 +1,10 @@
 <template>
   <div class="windows-create pt-3 border border-secondary rounded p-2 mb-2">
-    <i>Window ID </i>
-    <input v-model="id" />
-    <br>
     <i>Window Name </i>
     <input v-model="name" />
     <br>
     <i>Room ID </i>
     <input v-model="roomId" />
-    <br>
-    <i>Room Name </i>
-    <input v-model="roomName" />
     <br>
     <i>Window Status </i>
     <input v-model="windowStatus" />
@@ -37,32 +31,23 @@ export default {
   
   data: function() {
     return {
-      id:-"",
       name: "",
       roomId: "",
-      roomName: "",
       windowStatus: "",
       errors: []
     }
   },
 
-/*
-  created: async function() {
-    let response = await axios.get(`${API_HOST}/api/windows`);
-    let windows = response.data;
-    this.windows = windows;
-  },
-  */
   methods: {
    async createWindow() {
       try {
-        await axios.post(`${API_HOST}/api/windows`, {
-        id: this.id,
-        name: this.name,
-        roomId: this.roomId,
-        roomName: this.roomName,
-        windowStatus: this.windowStatus
-        })
+       let response = await axios.post(`${API_HOST}/api/windows`, {
+  "name": this.name,
+  "roomId": this.roomId,
+  "windowStatus": this.windowStatus
+  });
+        let updatedWindow = response.data;
+      this.$emit('window-updated', updatedWindow);
       } catch (e) {
         this.errors.push(e)
       }
@@ -71,5 +56,3 @@ export default {
 }
 
 </script>
-
-
